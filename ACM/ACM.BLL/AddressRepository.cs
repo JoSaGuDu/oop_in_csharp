b06f54f70fc35b25128b9eee3a6ca3ae92c23016
Jose <josegtuti5@gmail.com>
2019-10-31 06:47:22 -0400

    Basic relationship between classes stablished
    --Address list generation added
    --Address repository added
    --Customer, Address, AddressRepository and CostumerRepository relationship established.

---
+++ ACM/ACM.BLL/AddressRepository.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BLL
+{
+    public class AddressRepository
+    {
+        ///<sumary>
+        ///Retrieve one addres
+        ///<sumary>
+        public Address Retrieve(int addressId)
+        {
+            //Collaboration relationship: AddressRepository --USES A-- Address object to populate it with info from database.
+            Address address = new Address(addressId);
+            //TODO: code to get the address form the database
+
+            //Temporary address to work  with
+            if (addressId == 1)
+            {
+                address.StreetLine1 = "3350 Avenue Linton";
+                address.StreetLine2 = "App 207";
+                address.City = "Montreal";
+                address.State = "Quebec";
+                address.PostalCode = "H3S1T2";
+                address.Country = "Canada";
+                address.Type = 1;
+            }
+            return address;
+        }  
+        
+        ///<summary>
+        ///Retrieve all addresses for a coustumer
+        ///</summary>
+        public IEnumerable<Address>RetrieveByCustomerId(int coutmerId)
+        {
+            var AddressList = new List<Address>();
+
+            Address address = new Address(1)
+            {
+                StreetLine1 = "3350 Avenue Linton",
+                StreetLine2 = "App 207",
+                City = "Montreal",
+                State = "Quebec",
+                PostalCode = "H3S1T2",
+                Country = "Canada",
+                Type = 1
+            };
+            AddressList.Add(address);
+
+            address = new Address(2)
+            {
+                StreetLine1 = "400 Rue Montfort",
+                StreetLine2 = "Suit J2051",
+                City = "Montreal",
+                State = "Quebec",
+                PostalCode = "H4S2T3",
+                Country = "Canada",
+                Type = 2
+            };
+            AddressList.Add(address);
+            return AddressList;
+        }
+
+        ///<sumary>
+        ///Saves the current address
+        ///<sumary>
+        public bool save(Address address)
+        {
+            //Collaboration Relarionship: Repository --USES A-- entity class taking an instance of Address to use the instance information  to save info into a database
+
+            Address newAddress = new Address();
+            //TODO: code to save the new customer to the database
+            return true;
+        }
+    }
+}
--- ACM/ACM.BLL/CustomerRepository.cs
+++ ACM/ACM.BLL/CustomerRepository.cs
@@ -8,11 +8,22 @@ namespace ACM.BLL
 {
     public class CustomerRepository
     {
+        //Stablisshing a collaboration relationship with address repository to get sddress infromation
+        //2.Create the AddresRepositoryInstance by calling the constructor
+        public CustomerRepository() 
+        {
+            _addressRepository = new AddressRepository();
+        }
+
+        //1. Create a private property for the address repository object
+        private AddressRepository _addressRepository { get; set; }
+
         ///<sumary>
         ///Retrieve one customer
         ///<sumary>
         public Customer Retrieve (int customerID)
         {
+            //Collaboration relationship: CustomerRepository USES A Customer object to populate it wit info from database.
             Customer customer = new Customer(customerID);
             //TODO: code to get the costumer form the database
 
@@ -22,6 +33,7 @@ namespace ACM.BLL
                 customer.EmailAddress = "jsgd@jsgd.ca";
                 customer.FirstName = "Jose";
                 customer.LastName = "Gutierrez";
+                customer.AddressList = _addressRepository.RetrieveByCustomerId(customerID).ToList();
 
             }
 
@@ -44,6 +56,8 @@ namespace ACM.BLL
         ///<sumary>
         public bool save(Customer customer)
         {
+            //Collaboration Relarionship: Repository --USES A-- entity class taking an instance of Customer to use the instance information  to save info into a database
+
             Customer newCustomer = new Customer();
             //TODO: code to save the new customer to the database
             return true;
